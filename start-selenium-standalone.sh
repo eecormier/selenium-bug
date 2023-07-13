79311d4b20a87081ef35e0756fc89ea4c5806bb6
eecormier <139489032+eecormier@users.noreply.github.com>
2023-07-13 12:32:07 -0600

    Update start-selenium-standalone.sh

--- start-selenium-standalone.sh
+++ start-selenium-standalone.sh
@@ -4,5 +4,6 @@
 
 echo "Starting Selenium Grid Standalone..."
 java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar standalone \
+    --session-request-timeout 30 \
     --config /opt/selenium/config.toml \
     ${SE_OPTS}
