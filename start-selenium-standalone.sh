1aee136cb05bf51e52055b8347bc46db55489ac1
eecormier <139489032+eecormier@users.noreply.github.com>
2023-07-13 11:06:48 -0600

    Create start-selenium-standalone.sh

---
+++ start-selenium-standalone.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# IMPORTANT: Change this file only in directory Standalone!
+
+echo "Starting Selenium Grid Standalone..."
+java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar standalone \
+    --config /opt/selenium/config.toml \
+    ${SE_OPTS}
